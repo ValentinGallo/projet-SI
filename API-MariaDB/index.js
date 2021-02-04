a1ca350c6391d8352aa89f24453c2da9c2675add
killianLC <killianlecorre18@gmail.com>
2021-02-04 16:05:44 +0100

    Post creer User

--- API-MariaDB/index.js
+++ API-MariaDB/index.js
@@ -10,7 +10,7 @@ let connection = mysql.createConnection({
     database: process.env.DB_DATABASE
 });
 connection.connect();
-
+app.use(express.json())
 
 //////////////////
 //GET
@@ -18,7 +18,7 @@ connection.connect();
 
     //Selection global
     app.get('/user', (req,res) => {
-        connection.query('SELECT * from user', function (error, results) {
+        connection.query('SELECT * from users', function (error, results) {
             if (error) throw error;
                 console.log('result :', results);
                 res.status(200).json(results)
@@ -74,7 +74,8 @@ connection.connect();
     //Select dans la table USER
     app.get('/user/:id', (req,res) => {
         const id = parseInt(req.params.id)
-        connection.query('SELECT id FROM user WHERE id ='+id, function (error, results) {
+        
+        connection.query('SELECT users.id, identifiant, motDePasse, idRole, nom as nomRole FROM role INNER JOIN users ON role.id = users.idRole WHERE users.id='+id, function (error, results) {
             if (error) throw error;
                 console.log('result :', results);
                 res.status(200).json(results)
@@ -82,15 +83,32 @@ connection.connect();
        
     })
 
-    app.get('/user/motDePasse/:id', (req,res) => {
-        const id = parseInt(req.params.id)
-        connection.query('SELECT motdepasse FROM user WHERE id ='+id, function (error, results) {
+    //Post
+    app.post('/user', (req,res)=>{
+        const identifiant = req.body.identifiant
+        const motDePasse = req.body.motDePasse
+        const idRole = parseInt(req.body.idRole)
+
+        var sql = "INSERT INTO users (identifiant, motDePasse, idRole) VALUES ('"+identifiant+"','"+motDePasse+"','"+idRole+"')";
+        connection.query(sql, function (error, results) {
             if (error) throw error;
                 console.log('result :', results);
                 res.status(200).json(results)
         });
-       
     })
+
+    app.post('/role', (req,res)=>{
+        const nom = req.body.nom
+
+        var sql = "INSERT INTO role (nom) VALUES ('"+nom+"')";
+        connection.query(sql, function (error, results) {
+            if (error) throw error;
+                console.log('result :', results);
+                res.status(200).json(results)
+        });
+    })
+
+
 
 //////////////////
 //Serveur
