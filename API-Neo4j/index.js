6e44fa99b18392243e049fbcf352c32509bdcfbd
Yoann <contactyoannmc@gmail.com>
2021-02-12 14:23:58 +0100

    Test connexion - neo4j

--- API-Neo4j/index.js
+++ API-Neo4j/index.js
@@ -5,12 +5,16 @@ const app = express()
 const driver = neo4j.driver('bolt://obiwan2.univ-brest.fr:7687')
 const session = driver.session()
 
-app.get('/test', (req,res) => {
+app.get('/', (req,res) => {
     driver.verifyConnectivity()
-    .then((log) => {
-        res.send(log);
-    })
+    .then(() => {
+        res.send("Connexion Neo4j: ✔️");
+    }, () => {
+        res.send("Connexion Neo4j: ❌");
+    });
 })
+
+
 
 app.listen(7034, () => {
     console.log('Serveur à l\'écoute')
