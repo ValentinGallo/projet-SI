e3d280e9d353fc61e4d062ee9e12ab7723245e45
Yoann <contactyoannmc@gmail.com>
2021-02-17 18:38:20 +0100

    Ajouts - GET/POST/DELETE

--- API-Neo4j/index.js
+++ API-Neo4j/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const neo4j = require('neo4j-driver') 
 const app = express()
 
-const driver = neo4j.driver('bolt://obiwan2.univ-brest.fr:7687')
+const driver = neo4j.driver('bolt://localhost:7687')
 const session = driver.session()
 
+/* Vérification de la connexion à la BDD Neo4j */
 app.get('/', (req,res) => {
     driver.verifyConnectivity()
     .then(() => {
@@ -14,8 +15,136 @@ app.get('/', (req,res) => {
     });
 })
 
+/* Module de formation */
+app.get('/formation/:id', (req,res) => {
+    const id = parseInt(req.params.id)
 
+    session.run('MATCH (a:GRP2_formation {id: $id}) RETURN a', {id: id})
+    .then(function (result) {
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
 
+        res.send(node.properties);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+app.post('/formation/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    
+    session.run('CREATE (a:GRP2_formation {id: $id}) RETURN a', { id: id })
+    .then(function (result) {
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+
+        res.send(node.properties);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+app.delete('/formation/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    
+    session.run('MATCH (a:GRP2_formation {id: $id}) DELETE a', { id: id })
+    .then(function (result) {
+        res.status(200);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+/* Unité Pédagogique */
+app.get('/unite_pedagogique/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+
+    session.run('MATCH (a:GRP2_unite_pedagogique {id: $id}) RETURN a', {id: id})
+    .then(function (result) {
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+
+        res.send(node.properties);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+app.post('/unite_pedagogique/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    
+    session.run('CREATE (a:GRP2_unite_pedagogique {id: $id}) RETURN a', { id: id })
+    .then(function (result) {
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+
+        res.send(node.properties);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+app.delete('/unite_pedagogique/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    
+    session.run('MATCH (a:GRP2_unite_pedagogique {id: $id}) DELETE a', { id: id })
+    .then(function (result) {
+        res.status(200);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+/* Utilisateur */
+app.get('/utilisateur/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+
+    session.run('MATCH (a:GRP2_utilisateur {id: $id}) RETURN a', {id: id})
+    .then(function (result) {
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+
+        res.send(node.properties);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+app.post('/utilisateur/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    
+    session.run('CREATE (a:GRP2_utilisateur {id: $id}) RETURN a', { id: id })
+    .then(function (result) {
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+
+        res.send(node.properties);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+app.delete('/utilisateur/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    
+    session.run('MATCH (a:GRP2_utilisateur {id: $id}) DELETE a', { id: id })
+    .then(function (result) {
+        res.status(200);
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+/* Lancement du serveur */
 app.listen(7034, () => {
-    console.log('Serveur à l\'écoute')
+    console.log('--- SERVEUR NEO4J ---')
 })
