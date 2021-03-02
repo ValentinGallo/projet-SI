f7191429f192ef01a0799c35dd1a672e076bc06c
Yoann <contactyoannmc@gmail.com>
2021-03-02 19:41:37 +0100

    Modif - API Neo4j

--- API-Neo4j/index.js
+++ API-Neo4j/index.js
@@ -16,21 +16,6 @@ app.get('/', (req,res) => {
 })
 
 /* Module de formation */
-app.get('/formation/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-
-    session.run('MATCH (a:GRP2_formation {id: $id}) RETURN a', {id: id})
-    .then(function (result) {
-        const singleRecord = result.records[0]
-        const node = singleRecord.get(0)
-
-        res.send(node.properties);
-    })
-    .catch(function (error) {
-        res.status(404).send(error)
-    })
-})
-
 app.post('/formation/:id', (req,res) => {
     const id = parseInt(req.params.id)
     
@@ -59,21 +44,6 @@ app.delete('/formation/:id', (req,res) => {
 })
 
 /* Unité Pédagogique */
-app.get('/unite_pedagogique/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-
-    session.run('MATCH (a:GRP2_unite_pedagogique {id: $id}) RETURN a', {id: id})
-    .then(function (result) {
-        const singleRecord = result.records[0]
-        const node = singleRecord.get(0)
-
-        res.send(node.properties);
-    })
-    .catch(function (error) {
-        res.status(404).send(error)
-    })
-})
-
 app.post('/unite_pedagogique/:id', (req,res) => {
     const id = parseInt(req.params.id)
     
@@ -102,21 +72,6 @@ app.delete('/unite_pedagogique/:id', (req,res) => {
 })
 
 /* Utilisateur */
-app.get('/utilisateur/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-
-    session.run('MATCH (a:GRP2_utilisateur {id: $id}) RETURN a', {id: id})
-    .then(function (result) {
-        const singleRecord = result.records[0]
-        const node = singleRecord.get(0)
-
-        res.send(node.properties);
-    })
-    .catch(function (error) {
-        res.status(404).send(error)
-    })
-})
-
 app.post('/utilisateur/:id', (req,res) => {
     const id = parseInt(req.params.id)
     
@@ -144,7 +99,53 @@ app.delete('/utilisateur/:id', (req,res) => {
     })
 })
 
-/* RELATION */
+/* OBTENIR DES RELATIONS */
+app.get('/utilisateur/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+
+     // Permet d'obtenir les UP d'un utilisateur
+    session.run('MATCH (a:GRP2_utilisateur {id: $id})-[b]-(c) RETURN c', {id: id})
+    .then(function (results) {
+            var unite_pedagogique = []
+            results.records.forEach((record) => {
+                if (record._fields[0].properties.id != null) {
+                    unite_pedagogique.push({
+                        id: record._fields[0].properties.id,
+                    })
+                }
+            })
+
+            res.send(unite_pedagogique)
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+
+
+app.get('/formation/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+
+    // Permet d'obtenir les unités pédagogique d'un module de formation
+    session.run('MATCH (a:GRP2_formation {id: $id})-[b]-(c) RETURN c', {id: id})
+    .then(function (results) {
+        var module_formation = []
+        results.records.forEach((record) => {
+            if (record._fields[0].properties.id != null) {
+                module_formation.push({
+                    id: record._fields[0].properties.id,
+                })
+            }
+        })
+
+        res.send(module_formation)
+    })
+    .catch(function (error) {
+        res.status(404).send(error)
+    })
+})
+    
+/* CREER DES RELATIONS */
 app.post('/formation_up/:id_formation/:id_up', (req,res) => {
     const id_formation = parseInt(req.params.id_formation)
     const id_up        = parseInt(req.params.id_up)
