5cc80a010cf55568237c495996a0d85f55db7148
Valentin Gallo <valentingallo.pro@gmail.com>
2021-03-04 17:16:20 +0100

    fix css

--- app/src/Components/Appbar/Appbar.js
+++ app/src/Components/Appbar/Appbar.js
@@ -9,8 +9,8 @@ class Appbar extends React.Component {
 
 	render() {
           if (localStorage.getItem("id") == null) {
-          return <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">GRPN</a>
+          return <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+          <a className="navbar-brand" href="/" style={{marginLeft: '1rem'}}>GRPN</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
